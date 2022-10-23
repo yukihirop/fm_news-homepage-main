681f4a4ea7553f3d9598f2b5c3440349b1833322
yukihirop <te108186@gmail.com>
2022-10-24 00:48:41 +0900

    feat: 🎸 impl header

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,12 +1,87 @@
-import type { NextPage } from 'next'
-import styled from '@emotion/react'
+import type { NextPage } from "next";
+import styled from "@emotion/styled";
+import { sm } from "styles/media-query";
 
-const NewsContainer = styled.main``
+const NewsContainer = styled.main`
+  width: 100%;
+  height: 100%;
+  padding: 1rem;
+  border: solid 1px black;
+`;
+
+const NewsHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NewsLogo = styled.h1`
+  width: 70px;
+  height: 50px;
+  background-image: url("./images/logo.svg");
+  background-repeat: no-repeat;
+`;
+
+const NewsMenu = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  list-style: none;
+
+  ${sm} {
+    display: none;
+  }
+`;
+
+const NewsMenuMobile = styled.button`
+  width: 60px;
+  height: 60px;
+  display: none;
+
+  ${sm} {
+    display: block;
+    background-image: url("./images/icon-menu.svg");
+    background-repeat: no-repeat;
+    background-position-x: 50%;
+    background-position-y: 50%;
+
+    &:hover {
+      cursor: pointer;
+    } 
+  }
+`;
+
+const NewsMenuItem = styled.li`
+  margin-left: 3rem;
+
+  &:hover {
+    color: var(--soft-orange);
+  }
+`;
 
 const Home: NextPage = () => {
   return (
-    <main></main>
-  )
-}
+    <NewsContainer>
+      <NewsHeader>
+        <NewsLogo />
+        <NewsMenu>
+          <NewsMenuItem>
+            <a href="">Home</a>
+          </NewsMenuItem>
+          <NewsMenuItem>
+            <a href="">New</a>
+          </NewsMenuItem>
+          <NewsMenuItem>
+            <a href="">Popular</a>
+          </NewsMenuItem>
+          <NewsMenuItem>
+            <a href="">Categories</a>
+          </NewsMenuItem>
+        </NewsMenu>
+        <NewsMenuMobile/>
+      </NewsHeader>
+    </NewsContainer>
+  );
+};
 
-export default Home
+export default Home;
